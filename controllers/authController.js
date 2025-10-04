0d00149461499e2c503d66401b9ceeccb011b7ad
Ajayi Basit Adeola <hajayibasith1@gmail.com>
2025-10-04 02:01:12 +0100

    second commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 // src/controllers/authController.js
 
-const User = require('../models/User');
+const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
